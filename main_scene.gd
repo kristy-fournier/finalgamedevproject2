87c798a270e3c00ec2d2ca87c188a664d295dfc8
Mine60000 <ljmcanulty@gmail.com>
2025-03-12 15:35:35 -0400

    Reprogrammed Movement

--- main_scene.gd
+++ main_scene.gd
@@ -8,14 +8,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var tilemap = $"CurrentLevelContent/Level1/Floor A/Wall"
-	var character = $CurrentLevelContent/Character
-	if tilemap.get_cell_tile_data(tilemap.local_to_map(character.position)) != null:
-		if tilemap.get_cell_tile_data(tilemap.local_to_map(character.position)).get_custom_data("Name") == "wall":
-			character.position = character.lastPosition
-	var itemmap = $"CurrentLevelContent/Level1/Floor A/Items"
-	if itemmap.get_cell_tile_data(itemmap.local_to_map(character.position)) != null:
-		if itemmap.get_cell_tile_data(itemmap.local_to_map(character.position)).get_custom_data("Name") == "ladderUp":
-			$"CurrentLevelContent/Level1/Floor A".visible = false
-			$"CurrentLevelContent/Level1/Floor B".visible = true
-			
+	
+	#var character = $CurrentLevelContent/Character
+	#var itemmap = $"CurrentLevelContent/Level1/Floor A/Items"
+	#if itemmap.get_cell_tile_data(itemmap.local_to_map(character.position)) != null:
+		#if itemmap.get_cell_tile_data(itemmap.local_to_map(character.position)).get_custom_data("Name") == "ladderUp":
+			#$"CurrentLevelContent/Level1/Floor A".visible = false
+			#$"CurrentLevelContent/Level1/Floor B".visible = true
+	pass
+
+
+func _on_character_change_floors() -> void:
+	$"CurrentLevelContent/Level1/Floor A".visible = false
+	$"CurrentLevelContent/Level1/Floor B".visible = true

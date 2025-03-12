3ae4336aeab6bad00fbb6891e73cab8fa7154000
Kristy Fournier <124598538+kristy-fournier@users.noreply.github.com>
2025-03-12 10:54:00 -0400

    very basic working mainscene

--- character.gd
+++ character.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var lastPosition
 
 var current_floor
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
+	lastPosition = self.position
 	if Input.is_action_just_pressed("move_right"):
 		self.position = self.position + Vector2(-16,0)
 	if Input.is_action_just_pressed("move_left"):

6e45a5c51ef381c4c3187f4e8c895a0051634186
Mine60000 <ljmcanulty@gmail.com>
2025-03-15 15:34:37 -0400

    Fixed movement getting offset by decimal amounts

--- character.gd
+++ character.gd
@@ -3,7 +3,8 @@ var lastPosition
 
 var current_floor
 var desired_position
-var moving
+var moving: bool
+var overlaps: bool
 signal Change_floors
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,11 +15,13 @@ func _physics_process(delta: float) -> void:
 	var collision_info = move_and_collide(((desired_position-self.position))*1)
 	if collision_info:
 		desired_position = lastPosition
-	print(self.position.ceil())
+	print(self.position)
 	print(desired_position)
-	if desired_position.ceil() == self.position.ceil():
-		
+	if desired_position.round() == self.position.round():
+		self.position = self.position.round()
 		moving = false
+		if(overlaps):
+			_overlap_handler()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -38,4 +41,10 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	overlaps = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	overlaps = false
+
+func _overlap_handler() -> void:
 	Change_floors.emit()

1c15bb8deac1579e718e790ac9da16ac6fe409d9
Kristy Fournier <124598538+kristy-fournier@users.noreply.github.com>
2025-03-16 20:08:13 -0400

    Made Level 2(?) plus a level root node for future levels

---
+++ level.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var floorOrder
+@export var startingFloor:Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# set this to the node of the starting floor
+	# startingFloor = $"Floor A"
+	for i in self.get_children():
+		if i != startingFloor:
+			i.visible = false
+	# initialise the floor order
+	floorOrder = []
+	for i in self.get_children():
+		floorOrder.append(i)
+	floorOrder.reverse()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+	
